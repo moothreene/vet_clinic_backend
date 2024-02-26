463b59608590c1078b0212366996ee782e07e0fe
Martin <moothreene@gmail.com>
2024-02-26 21:37:00 +0300

    pet schema and model

---
+++ models/Pet.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const PetSchema = new mongoose.Schema({
+    owner_id:{type:mongoose.Schema.Types.ObjectId, ref:"User",required:true},
+    name:{type:String,required:true,min:2},
+    sex:{type:String, required:true},
+    weight:{type:Number},
+    birthday:{type:Date,required:true},
+    species:{type:String, required:true, min:3},
+    color:{type:String},
+})
+
+const PetModel = mongoose.model("Pet",PetSchema,"Pets");
+
+module.exports = PetModel;
