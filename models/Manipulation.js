2a11d5b9bff264aa807e012245ac25046d3f7bca
Martin <moothreene@gmail.com>
2024-03-19 12:43:54 +0300

    Update Manipulation.js

--- models/Manipulation.js
+++ models/Manipulation.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose");
 const ManipulationSchema = new mongoose.Schema({
     pet_id:{type:mongoose.Schema.Types.ObjectId,ref:"Pet",required:true},
     date:{type:Date,required:true},
-    weight:{type:Number},
-    temp:{type:Number},
+    weight:{type:mongoose.Types.Decimal128},
+    temp:{type:mongoose.Types.Decimal128},
     purpose:{type:String},
     doctor:{type:mongoose.Schema.Types.ObjectId,ref:"User",required:true},
     desc:{type:String},
