ba642bcaba51e4fc8fa6b1e275a7870e71bbb0e9
Martin <moothreene@gmail.com>
2024-03-23 19:05:45 +0300

    bringing the number type back because of errors

--- models/Manipulation.js
+++ models/Manipulation.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose");
 const ManipulationSchema = new mongoose.Schema({
     pet_id:{type:mongoose.Schema.Types.ObjectId,ref:"Pet",required:true},
     date:{type:Date,required:true},
-    weight:{type:mongoose.Types.Decimal128},
-    temp:{type:mongoose.Types.Decimal128},
+    weight:{type:Number},
+    temp:{type:Number},
     purpose:{type:String},
     doctor:{type:mongoose.Schema.Types.ObjectId,ref:"User",required:true},
     desc:{type:String},
